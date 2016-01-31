9cb136b588c05004bde67b2819d771b94edae936
sam <samuel1btoc@gmail.com>
2016-01-31 21:52:14 +0100

    arreglar fechar Archive admin tb

--- DTTCore.php
+++ DTTCore.php
@@ -13,7 +13,8 @@
 	} catch (Exception $e) {
         die("No se pudo conectar: " . $e->getMessage());
 	}
-	
+    session_name("DTT");
+    session_start();
 	
 	
     function display($row){
@@ -56,4 +57,48 @@
         }
    
    }
+/*function login($user, $password){ 
+
+    $error=0;
+    if (!empty($user) && !empty($password)){
+
+     global $pdo; 
+     $query = $pdo->prepare("SELECT * FROM users WHERE user='$user'"); //Busco el user
+     $query->execute();
+     $num = $query->rowCount(); 
+
+     if($num!=0){
+        $row = $query->fetch()
+
+            //Ahora que se que existe el user compruebo el password
+        if(password_verify($password, $row["password"])){
+
+               //Creo la sesion asignando variables de sesion con los datos del usuario
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["name"] = $row["name"];
+            $_SESSION["user"] = $row["user"];
+            $_SESSION['timeout'] = time();
+            
+         }else{ $error="Wrong Password"; }
+         
+     }else{ $error="user not in the system"; }
+
+         //Libero de la memoria los resultados de la consulta
+         mysqli_free_result($result);
+
+    }else{ $error=-1; }
+
+      return $error;
+
+}*/
+
+function bcrypt($password){ 
+    $options = array('cost' => 10);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT, $options);
+    return $password_hash;
+}
+
+function logout(){ 
+    session_destroy();
+}
    
--- DTTadmin.php
+++ DTTadmin.php
@@ -1,18 +1,37 @@
 <?php
     require_once("DTTCore.php");
-    //if(!isset($_SESSION["id"]){
-        $template = file_get_contents("DTTHeader.html");
-	    echo setTitle($template);
-	    require_once("logIn.html");
-        //if(login()){}
-    //}else{
+    $action = $_GET["a"];
+    //we dont have sesion yet
+    $template = file_get_contents("DTTHeader.html");
+    echo setTitle($template);
     
+    if($action==null){
+        $query = $pdo->prepare("INSERT INTO 'users'('name', 'password') VALUES ('samuel',bcrypt('DTTemployee'))");
+        $query->execute();
+        header("Location: DTTadmin.php?a=login");
+        
+    }else if($action=="login"){
     
-    //}
+        require_once("logIn.html");
+        
+    }else if($action=="check"){
 
+        $user=$_POST["user"];
+        $pass=$_POST["password"];
 
+        $_SESSION["error"]=login($user, $pass);
+        header("Location: DTTadmin.php?a=login");
 
+   //Pagina de registro
+    }else if($action=="admin"){
+    
+        if(!isset($_SESSION["id"])){
 
-
+        }else{
+	        echo file_get_contents("widgetAdmin.html");
+        }
+    }else{
+        header("Location: DTTadmin.php?a=login");
+    }
 	echo "<a href='index.php'>building</a>";
 
