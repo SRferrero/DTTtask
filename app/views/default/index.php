291709d27e36352d75b4d13675f4a03725049ac2
heil Biach <351256@student.fontys.nl>
2016-02-07 18:01:41 +0100

    mvc framework

--- app/views/default/index.php
+++ app/views/default/index.php
@@ -1,9 +1,9 @@
 <?php
-    require_once(config.php);
+    require_once("config.php");
 
-    
     $url = (isset($_GET['url'])) ? $_GET['url'] : 'Index/index';
     $url = explode ("/", $url);
     $controller = array_shift($url);
     $method = array_shift($url);
     $params = array($url);
+    echo "here" . $controller . $method . $params;
