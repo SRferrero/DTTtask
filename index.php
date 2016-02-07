51264d0eb7ea0f2c9fe348d7cc74fd38c1d40ad5
sam <samuel1btoc@gmail.com>
2016-02-07 19:59:18 +0100

    htaccess

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
+
+    $url = $_GET['url'];
+    var_dump($url);
+/*
     require_once("config.php");
 
-    $url = (isset($_GET['url'])) ? $_GET['url'] : 'Index/index';
+    $url = (isset($_GET['url'])) ? $_GET['url'] : 'Index/lol/dfg';
     $url = explode ("/", $url);
     $controller = array_shift($url);
     $method = array_shift($url);
-    $params = array($url);
-    echo "here" . $controller . $method . $params;
+    $params = $url;
+    echo dirname(__FILE__);
+    echo "heresad" . $controller . $method ;*/
